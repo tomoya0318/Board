446923888123e503a8836cd75c2a4e6363f6f85a
tomoya-n <s266227@wakayama-u.ac.jp>
2024-09-22 03:01:27 +0900

    Change password to hash

--- backend/db/migrate/20240921175632_create_users.rb
+++ backend/db/migrate/20240921175632_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :name, null: false
-      t.string :password, null: false
+      t.string :name
 
       t.timestamps
     end
