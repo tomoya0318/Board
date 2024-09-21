cbb27c1d150f3ecef091c3a3510a0430f1744dab
tomoya-n <s266227@wakayama-u.ac.jp>
2024-09-22 02:44:21 +0900

    Added model and controller for User management

---
+++ backend/db/migrate/20240921173544_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :password, null: false
+
+      t.timestamps
+    end
+  end
+end
