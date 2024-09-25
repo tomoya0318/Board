d99808158e234b8fb778ee33f787111f62bbdbeb
tomoya0318 <134858551+tomoya0318@users.noreply.github.com>
2024-09-25 10:56:12 +0900

    テストDBを自動で作成するように変更 (#4)
    * Update gemfile to solve the error
    
    * Added model and controller for User management
    
    * Change password to hash
    
    * add test-db and test for rails
    
    * Change development environment to work with devcontainer
    
    * test-db is created at build time

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,33 +1,32 @@
-#!/bin/sh
-
-set -e
-
+#!/bin/bash
+   set -e
 # サーバーのPIDファイルを削除
 rm -f /backend/tmp/pids/server.pid
-
 # データベースが利用可能になるのを待つ関数
 wait_for_db() {
-  echo "Waiting for database to become available..."
-  while ! pg_isready -h db -p 5432 -q -U postgres; do
-    sleep 2
-  done
-  echo "Database is available."
+ echo "Waiting for database to become available..."
+ while ! pg_isready -h db -p 5432 -q -U postgres; do
+   sleep 2
+ done
+ echo "Database is available."
 }
 
 # データベースのセットアップ
 setup_database() {
-  echo "Setting up database..."
-  bundle exec rails db:create
-  bundle exec rails db:migrate
-  echo "Database setup completed."
+ echo "Setting up database..."
+ bundle exec rails db:create
+ bundle exec rails db:migrate
+ RAILS_ENV=test bundle exec rails db:create
+ RAILS_ENV=test bundle exec rails db:migrate
+ echo "Database setup completed."
 }
 
 # メイン処理
 main() {
-  wait_for_db
-  setup_database
-  echo "Executing command: $@"
-  exec "$@"
+ wait_for_db
+ setup_database
+ echo "Executing command: $@"
+ exec "$@"
 }
 
 main "$@"
