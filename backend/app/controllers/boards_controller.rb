f6be0cb0ad3844fa23636f7a48f521bbf4041f40
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-10-17 17:02:38 +0900

    不要な行を削除

--- backend/app/controllers/boards_controller.rb
+++ backend/app/controllers/boards_controller.rb
@@ -18,15 +18,12 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
   end
+  
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    @board = Board.find(params[:id])
-
     if @board.update(title: board_params[:title])
       redirect_to boards_path
     else
