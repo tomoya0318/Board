c33ab054852b4d59ae7d68d5b64981eaa51348da
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-10-17 12:35:22 +0900

    Categoryのルーティングと説明追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  #board
   get '/boards', to: 'boards#index', as: 'boards'
   get '/boards/new', to: 'boards#new', as: 'new_board'
-  post '/boards', to: 'boards#create'
+  post '/boards', to: 'boards#create', as: 'create_board'
   get '/boards/:id', to: 'boards#show', as: 'board'
   get '/boards/:id/edit', to: 'boards#edit', as: 'edit_board'
-  patch '/boards/:id', to: 'boards#update'
-  delete '/boards/:id', to: 'boards#destroy'
+  patch '/boards/:id', to: 'boards#update', as: 'update_board'
+  delete '/boards/:id', to: 'boards#destroy', as: 'destroy_board'
+  #category
+  get '/boards/:board_id/categories', to: 'categories#index', as: 'board_categories'
+  get '/boards/:board_id/categories/new', to: 'categories#new', as: 'new_board_category'
+  post '/boards/:board_id/categories', to: 'categories#create', as: 'create_board_category'
+  get '/boards/:board_id/categories/:id', to: 'categories#show', as: 'board_category'
+  get '/boards/:board_id/categories/:id/edit', to: 'categories#edit', as: 'edit_board_category'
+  patch '/boards/:board_id/categories/:id', to: 'categories#update', as: 'update_board_category'
+  delete '/boards/:board_id/categories/:id', to: 'categories#destroy', as: 'destroy_board_category'
 end
